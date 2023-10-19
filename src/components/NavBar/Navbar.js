8c84d48d366adeb55bc23dcfab5a870d778f5242
Hamza7865592 <89345113+Hamza7865592@users.noreply.github.com>
2023-10-20 00:48:32 +0500

    Update Navbar.js

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
        <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={600} className='MenuListItems'>Home</Link>
        <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-80} duration={600} className='MenuListItems'>About</Link>
        <Link activeClass='active' to='Projects' spy={true} smooth={true} offset={-50} duration={600} className='MenuListItems'>Projects</Link>
-       <Link activeClass='active' to='contactPage' spy={true} smooth={true} offset={-100} duration={600} className='MenuListItems'>Contact Me</Link>    
+       <Link activeClass='active' to='contactPage' spy={true} smooth={true} offset={-100} duration={600} className='MenuListItems'>Clients</Link>    
        </div> 
        <button className='MenuButton'>
            <img src={contactImg} alt='contact' className='MenuImg' onClick={()=>{document.getElementById('contactPage').scrollIntoView({behavior:'smooth'})}}/>Contact Me
